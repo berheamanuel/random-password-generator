62a99662d7484193691cae4fafff74f98bf26a6c
Amanuel <amanueleman@gmail.com>
2023-10-04 00:19:11 -0400

    Psudo code added

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+function generatePassword(){
+// prompts the user for password criteria
+// prompted for password criteria I select which criteria to include
+// length of the password: >8 and < 120 characters
+// character types:lowercase, uppercase, numeric, and/or special characters
+// input should be validated and at least one character type should be selected
+
+
+
+// displayed password
+return "password here.";
+
+}
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
