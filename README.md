6e8331b7c6bccd7cf7dd94bb2c4ba56278f6de59
Amanuel <amanueleman@gmail.com>
2023-10-06 22:35:33 -0400

    Added generatePassword function

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
-## Random Password Generator
+# Random Password Generator
+
+## Description
+
+This application emphasizes the use of Javascript to make dynamic changes to an HMTL document, so that to generate a random, secure, strong and unpredictable password for the user accounts.
+
+## Installation
+
+What are the steps required to install your project? Provide a step-by-step description of how to get the development environment running.
+
+## Usage
+
+
+
+    ```md
+    ![screenshot](assets/images/screenshot.png)
+    ```
+
+## References
+
+
+## License
+
+MIT License
+
+
+
--- script.js
+++ script.js
@@ -2,16 +2,70 @@
 var generateBtn = document.querySelector("#generate");
 
 function generatePassword(){
-// prompts the user for password criteria
-// prompted for password criteria I select which criteria to include
-// length of the password: >8 and < 120 characters
-// character types:lowercase, uppercase, numeric, and/or special characters
-// input should be validated and at least one character type should be selected
 
+  // create arraay variables to store character types/password criteria
+  var characters = {
+    lowerCaseLetters: "abcdefghijklmnopqrstuvwxyz".split(""),
+    upperCaseLetters: "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split(""),
+    specialChars: "!#$%&'()*+,-./\:;<=>?@[]^_`{|}~".split(""),
+    numbers: "0123456789".split(""),
 
+    // Initialize array for possible password characters
+    choosenChars: [],
 
-// displayed password
-return "password here.";
+    // Initialize array for generated password
+    passGenerated: [],
+
+    // prompt to ask the user to enter password length between 8 and 128
+    passLength: parseInt(prompt("Please enter the length of the password between 8 and 128.")),
+
+    // Prompt to ask user to confirm character type
+    selectLower: confirm("Will you include Lower Case Letters?"),
+    selectUpper: confirm("Will you include Upper Case Letters?"),
+    selectSpecialChars: confirm("Will you include Special Characters?"),
+    selectNumbers: confirm("Will you include Numbers?"),
+  };
+
+      // Add password characters to array of choices based on user input
+      if (characters.selectLower) {
+        characters.choosenChars = characters.choosenChars.concat(characters.lowerCaseLetters);
+      }
+      if (characters.selectUpper) {
+        characters.choosenChars = characters.choosenChars.concat(characters.upperCaseLetters);
+      }
+      if (characters.selectSpecialChars) {
+        characters.choosenChars = characters.choosenChars.concat(characters.specialChars);
+      }
+      if (characters.selectNumbers) {
+        characters.choosenChars = characters.choosenChars.concat(characters.numbers)
+      }
+
+  // validated user imput
+  if (characters.choosenChars.length) {
+      // validate lenght of the pasword
+      if (characters.passLength < 8 || characters.passLength > 128 || isNaN(characters.passLength)) {
+          alert("Please enter a password length between 8 and 128 characters.");
+          return "";
+
+      } else {
+            // Generate random characters to add to password
+            for (var i = 0; i < characters.passLength; i++) {
+                var randCharIndex =Math.floor(Math.random() * characters.choosenChars.length);
+                characters.passGenerated.push(characters.choosenChars[randCharIndex]);
+                
+            }
+                
+            // display password
+            var password = characters.passGenerated.join("");
+            return password;
+      }
+    
+  }else {
+      // Alert if no character type is selected
+      alert("Please select at least one charater type.");
+      return "";
+  }
+
 
 }
 
